d20cb16eb653976387180d5af82a418ac87ed611
PierreQuintero-eventtia <pierre@eventtia.com>
2023-07-03 15:02:47 -0400

    botn finish

--- app/src/main/java/com/example/dynamicfragment/MainActivity.java
+++ app/src/main/java/com/example/dynamicfragment/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.dynamicfragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.dynamicfragment.databinding.ActivityMainBinding;
+import com.example.dynamicfragment.databinding.FragmentQuestionBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view= binding.getRoot();
+        setContentView(view);
+
+        binding.killApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
 
