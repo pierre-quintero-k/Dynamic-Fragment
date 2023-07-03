a24d173eb8224c4fe23fb8072cfba6c11f612501
PierreQuintero-eventtia <pierre@eventtia.com>
2023-07-03 14:30:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dynamicfragment/QuestionFragment.java
@@ -0,0 +1,96 @@
+package com.example.dynamicfragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+import com.example.dynamicfragment.databinding.FragmentQuestionBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuestionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuestionFragment extends Fragment {
+    FragmentQuestionBinding binding;
+    public static final int YES=0;
+    public static final int NO=1;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public QuestionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment QuestionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static QuestionFragment newInstance(String param1, String param2) {
+        QuestionFragment fragment = new QuestionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentQuestionBinding.inflate(inflater, container, false);
+        View view= binding.getRoot();
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioSi:
+                        binding.textoConozco.setText(R.string.yes_message);
+                        break;
+                    case R.id.radioNo:
+                        binding.textoConozco.setText(R.string.no_message);
+                        break;
+                    default:
+                        binding.textoConozco.setText(R.string.question_one);
+                }
+            }
+        });
+    }
+}
+
